931b69394f91155bad9031be8ad4ccc3ebdfae5a
Oscar Fonts <oscar.fonts@gmail.com>
2016-10-10 08:56:56 +0200

    Add tags to run scripts; don't update base packages

--- 3.0.5/run.sh
+++ 3.0.5/run.sh
@@ -1,2 +1,2 @@
-docker run -v $PWD/data_dir:/var/local/geonetwork --name geonetwork -d -p 8888:8080 oscarfonts/geonetwork
+docker run -v $PWD/data_dir:/var/local/geonetwork --name geonetwork -d -p 8888:8080 oscarfonts/geonetwork:3.0.5
 docker logs -f geonetwork 2>&1
